894fe3bb010bc0f519736b9db78eb7034697c5fa
Girish <girishproject28@gmail.com>
2025-02-06 19:19:53 +0000

    initial

--- MFM_extractor/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# MFM_extractor/models/__init__.py
-MODEL_REGISTRY = {
-    "1": {
-        "name": "MERT-v0",
-        "module": "MFM_extractor.models.mert_v0_extractor",
-        "class": "MertV0Extractor"
-    }
-}
